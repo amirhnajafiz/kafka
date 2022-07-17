fd5be2f21a0b04e589e0fee93ea064af05a433ba
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-17 09:56:42 +0430

    remove: extra files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>
+      Hello
+    </h1>
   </main>
 </template>
