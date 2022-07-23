a1ecd066926307f1841d57f54d3022460bb99711
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-23 09:23:30 +0430

    remove: store

--- front-end/src/stores/information.js
+++ front-end/src/stores/information.js
@@ -1,15 +1,11 @@
 import { defineStore } from 'pinia'
 
-import data from '../locals/data.json'
-
 export const useInformationStore = defineStore({
   id: 'information',
   state: () => ({
-    data: data
+
   }),
   getters: {
-    getFirstName: (state) => state.data.firstname,
-    getLastName: (state) => state.data.lastname,
-    getAge: (state) => state.data.age,
+
   },
 })
