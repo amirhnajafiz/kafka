41298f12a95bbb32755b63c941d86df8db9a7b0f
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-23 09:19:51 +0430

    update: home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '@/views/Home/index.vue'
+import HomeView from '@/views/HomeView/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
