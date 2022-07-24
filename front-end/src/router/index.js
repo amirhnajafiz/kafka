f6b6195fface4987c603feba840bdac15869ca12
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-24 09:35:19 +0430

    add: routes

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// importing views
 import HomeView from '@/views/HomeView/index.vue'
+import AboutView from "@/views/AboutView/index.vue";
+import EducationView from "@/views/EducationView/index.vue";
+import ContactView from "@/views/ContactView/index.vue";
+import ProjectsView from "@/views/Project/ProjectsView/index.vue";
+import ProjectView from "@/views/Project/ProjectView/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +15,31 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView
+    },
+    {
+      path: "/edu",
+      name: "edu",
+      component: EducationView
+    },
+    {
+      path: "/contact",
+      name: "contact",
+      component: ContactView
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: ProjectsView
+    },
+    {
+      path: "/project/:id",
+      name: "project",
+      component: ProjectView
     }
   ]
 })
