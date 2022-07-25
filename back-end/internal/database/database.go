35b0f7bb0c796c7e2bbdd1e3c8316ea58d93dc4f
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-25 12:04:05 +0430

    add: database connection

--- back-end/internal/database/database.go
+++ back-end/internal/database/database.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func NewConnection(url string) (*mongo.Client, error) {
+	ctx := context.Background()
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongo: %w", err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("failed to ping mongo: %w", err)
+	}
+
+	return client, nil
+}
