e41c40a367f9aafff49f8726ff1796bf158bc108
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-25 12:21:48 +0430

    add: migrations

--- back-end/internal/model/project.go
+++ back-end/internal/model/project.go
@@ -1,7 +1,6 @@
 package model
 
 type Project struct {
-	Id          int    `bson:"id,omitempty"`
 	Title       string `bson:"title,omitempty"`
 	Description string `bson:"description,omitempty"`
 	Link        string `bson:"link,omitempty"`
