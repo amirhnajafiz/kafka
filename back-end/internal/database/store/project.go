1dc491fcc71651f85adef9d0c9bf3f21cd2b26a0
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-26 12:18:00 +0430

    init: project store

--- back-end/internal/database/store/project.go
+++ back-end/internal/database/store/project.go
@@ -1,1 +1,29 @@
 package store
+
+import (
+	"context"
+
+	"github.com/amirhnajafiz/personal-website/back-end/internal/model"
+)
+
+const (
+	projectsCollection = "projects"
+)
+
+type ProjectsCollection struct{}
+
+func (p *ProjectsCollection) Upsert(c context.Context, project *model.Project) error {
+	return nil
+}
+
+func (p *ProjectsCollection) GetAll(c context.Context) []*model.Project {
+	return nil
+}
+
+func (p *ProjectsCollection) GetAllAvailable(c context.Context) []*model.Project {
+	return nil
+}
+
+func (p *ProjectsCollection) GetSingle(c context.Context) *model.Project {
+	return nil
+}
