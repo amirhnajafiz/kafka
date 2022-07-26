51eac5ad278c38b6450e8394a021542890bc9c59
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-26 19:50:00 +0430

    refactor: handlers

--- back-end/internal/http/handler/projects.go
+++ back-end/internal/http/handler/projects.go
@@ -8,17 +8,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-func (h *Handler) GetAllProjects(c *fiber.Ctx) error {
-	filter := bson.D{{}}
-
-	projects, err := h.ProjectsCollection.GetAll(c.Context(), filter)
-	if err != nil {
-		return c.SendString(err.Error())
-	}
-
-	return c.JSON(projects)
-}
-
 func (h *Handler) UpsertProject(c *fiber.Ctx) error {
 	type request struct {
 		Title       string `json:"title"`
@@ -54,4 +43,39 @@ func (h *Handler) RemoveProject(c *fiber.Ctx) error {
 	}
 
 	return c.SendStatus(http.StatusAccepted)
+}
+
+func (h *Handler) GetAllProjects(c *fiber.Ctx) error {
+	filter := bson.D{{}}
+
+	projects, err := h.ProjectsCollection.GetAll(c.Context(), filter)
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+
+	return c.JSON(projects)
+}
+
+func (h *Handler) GetVisibleProjects(c *fiber.Ctx) error {
+	filter := bson.M{
+		"visible": bson.M{
+			"$eq": true,
+		},
+	}
+
+	p, err := h.ProjectsCollection.GetAll(c.Context(), filter)
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+
+	return c.JSON(p)
+}
+
+func (h *Handler) GetProjectById(c *fiber.Ctx) error {
+	p, err := h.ProjectsCollection.GetSingle(c.Context(), c.Params("title"))
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+
+	return c.JSON(p)
 }
