81454853abac59f6ba15bb1c190eedcd5eee9b35
amirhnajafiz <najafizadeh21@gmail.com>
2022-07-27 11:43:16 +0430

    fix: header

--- front-end/src/components/Header.vue
+++ front-end/src/components/Header.vue
@@ -1,17 +1,50 @@
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
+  <header class="header">
+    <nav class="nav">
+      <RouterLink class="select" to="/">Amirhossein Najafizade</RouterLink>
+      <div>
+        <RouterLink class="link select" to="/">Projects</RouterLink>
+        <RouterLink class="link select" to="/">Educations</RouterLink>
+        <RouterLink class="link select" to="/">About</RouterLink>
+        <RouterLink class="link select" to="/">Contact</RouterLink>
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
 export default {
-  name: "Header"
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
+a {
+  text-decoration: none;
+}
 
+.header {
+  width: 100%;
+}
+
+.nav {
+  width: 60%;
+  margin: 0 auto;
+  padding: 0 0 10px;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  border-bottom: 1px solid #ffffff;
+}
+
+.link {
+  margin: 0 10px;
+}
+
+.select:hover {
+  color: #aaaaaa;
+}
 </style>
